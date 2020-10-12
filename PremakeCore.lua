885f75163581515c73f8d8913946e60d49e3458c
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2020-10-12 08:21:11 +0100

    Separating c# projects to seperate file
    Fixes some issues with the build system

---
+++ PremakeCore.lua
@@ -0,0 +1,187 @@
+
+BuildTargetPath = "Build/Binaries/%{prj.name}-%{cfg.buildcfg}"
+BuildObjectPath = "Build/Objects/%{prj.name}-%{cfg.buildcfg}"
+
+newoption 
+{
+    trigger     = "renderer",
+    value       = "API",
+    description = "Choose a particular rendering backend",
+    default     = "opengl",
+    allowed = {
+       { "vulkan",    "Vulkan 1.2" },
+       { "gles",  "OpenGL ES 2.0" },
+    }
+}
+newoption
+{
+    trigger     = "audio-backend",
+    value       = "API",
+    description = "Choose a particular audio backend",
+    default     = "pulse",
+    allowed     = {
+        { "pulse", "Pulse Audio 13.99.1" },
+    }
+}
+newoption
+{
+    trigger     = "scripting-language",
+    value       = "API",
+    description = "Choose a particular scripting language",
+    default     = "none",
+    allowed     = {
+        { "none", "Disables scripting" },
+        { "mono", "Mono C# 6 <" },
+    }
+}
+
+-- Table of libraries that should be linked againsed
+ConfigurationLinks = {}
+-- Renderers
+ConfigurationLinks["vulkan"]        = "vulkan"
+ConfigurationLinks["gles"]          = "glad"
+-- Audio
+ConfigurationLinks["pulse"]         = "pulse"
+
+-- Table of macros that should be defined
+ConfigurationDefines = {}
+-- Renderers
+ConfigurationDefines["vulkan"]  = "MAGE_VULKAN_BACKEND"
+ConfigurationDefines["gles"]    = "MAGE_GLES_BACKEND"
+-- Audio
+ConfigurationDefines["pulse"]   = "MAGE_PULSE_AUDIO_BACKEND"
+
+-- Table of files to include
+ConfigurationFiles  = {}
+ConfigurationFiles["vulkan"]    = "Mage/Source/Vulkan/**.*"
+ConfigurationFiles["gles"]      = "Mage/Source/OpenGL/**.*"
+ConfigurationFiles["pulse"]     = "Mage/Source/Pulse-Audio/**.*"
+ConfigurationFiles["mono"]      = "Mage/Source/Mono/*.cs"
+
+
+workspace "Mage"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+    }
+    startproject "Sandbox"
+
+
+
+
+group "Externals"
+if _OPTIONS["renderer"] == "gles" then
+    include "Mage/Externals/glad/"
+end
+include "Mage/Externals/stb-image"
+include "Mage/Externals/glfw3"
+
+-- Engine Project
+project "MageEngine"
+    location "Mage"
+    kind "StaticLib"
+    language "C"
+    
+    targetdir (BuildTargetPath)
+    objdir (BuildObjectPath)
+
+    files
+    {
+        -- Files all systems use 
+        "Mage/Source/*.h",
+        "Mage/Source/*.c",
+        "Mage/Source/Maths/*.h",
+        "Mage/Source/Maths/*.c",
+        "Mage/Source/ECS/*.h",
+        "Mage/Source/ECS/*.c",
+        ConfigurationFiles[_OPTIONS["renderer"]],
+        ConfigurationFiles[_OPTIONS["audio-backend"]],
+        ConfigurationFiles[_OPTIONS["scripting-language"]]   
+    }
+    defines
+    {
+        "MAGE_CORE",
+        "MAGE_ASSERTS",
+        ConfigurationDefines[_OPTIONS["renderer"]],
+        ConfigurationDefines[_OPTIONS["audio-backend"]],
+    }
+    includedirs
+    {
+        "Mage/Source/",
+    }
+    flags
+    {
+        "FatalWarnings",
+        "FatalCompileWarnings",
+        "FatalLinkWarnings",
+    }
+
+filter "system:linux"
+    pchheader "Mage/Source/mageAPI.h"
+    pchsource "Mage/Source/mageAPI.c"
+
+    files
+    {
+        "Mage/Source/Linux/*.h",
+        "Mage/Source/Linux/*.c",
+    }
+    
+filter "configurations:Debug"
+    defines "MAGE_DEBUG"
+    runtime "Debug"
+    symbols "On"
+    
+
+filter "configurations:Release"
+    defines "MAGE_RELEASE"
+    runtime "Release"
+    optimize "Speed"
+
+
+project "Sandbox"
+    location "%{prj.name}"
+    kind "ConsoleApp"
+    language "C"
+    targetdir (BuildTargetPath)
+    objdir (BuildObjectPath)
+
+    includedirs
+    {
+        "Mage/Source",
+    }
+    
+    files
+    {
+        "%{prj.name}/**.h",   
+        "%{prj.name}/**.c",
+    }
+
+    links
+    {
+        "MageEngine",
+        "GLFW",
+        "stb-image",
+        ConfigurationLinks[_OPTIONS["renderer"]],
+        ConfigurationLinks[_OPTIONS["audio-backend"]],
+    }
+    
+    filter "system:linux"
+        links 
+        { 
+            "dl",
+            "pthread",
+            "m",
+        }
+
+    filter "configurations:Debug"
+        defines "SANDBOX_DEBUG"    
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "SANDBOX_RELEASE"
+        runtime "Release"
+        optimize "Speed"
