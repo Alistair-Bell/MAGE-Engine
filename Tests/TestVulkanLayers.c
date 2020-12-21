7921cbabd5d6f97df20c2446e339103488bcceac
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2020-12-21 17:34:55 +0000

    Adding new test

--- Tests/TestVulkanLayers.c
+++ Tests/TestVulkanLayers.c
@@ -3,6 +3,11 @@
 
 static U8 TestValidationLayers(U0 *data)
 {
+    const char *layers[] = { VK_KHR_SURFACE_EXTENSION_NAME };
+    return MageVulkanRendererValidateExtensionsPresent(layers, sizeof(layers) / sizeof(const char *));
+}
+static U8 TestInstanceExtensions(U0 *data)
+{
     const char *extensions[] = { VK_KHR_SURFACE_EXTENSION_NAME };
     return MageVulkanRendererValidateExtensionsPresent(extensions, sizeof(extensions) / sizeof(const char *));
 }
@@ -19,6 +24,13 @@ I32 main()
     tests[0].ExpectedResult         = MageTrue;
     tests[0].Assertions             = MageFalse;
     tests[0].Callback               = TestValidationLayers;
+
+    tests[0].TestName               = "`Unit Tests` - vulkan instance extensions test";
+    tests[0].FailMessage            = "`Vulkan Instance Extensions` module has failed";
+    tests[0].ProgramData            = NULL;
+    tests[0].ExpectedResult         = MageTrue;
+    tests[0].Assertions             = MageFalse;
+    tests[0].Callback               = TestInstanceExtensions;
 
     MageUnitTestRuntimeInfo runtime;
     return MageUnitTestRunTests(tests, testCount, &runtime);
