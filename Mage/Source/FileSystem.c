cf292785b8a9de895519c44472e52b47557eec59
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2020-12-24 21:01:25 +0000

    Filesystem update

--- Mage/Source/FileSystem.c
+++ Mage/Source/FileSystem.c
@@ -4,19 +4,25 @@ U8 MageFileSystemCreate(MageFileSystemCreateInfo *info, MageFileSystem *system)
 {
     memset(system, 0, sizeof(MageFileSystem));
     system->Mounted = calloc(0, sizeof(const char *));
+    system->NextIndex = UINT32_MAX;
     return MageTrue;
 }
 U8 MageFileSystemMountDirectory(MageFileSystem *system, const char *mountPoint, U32 *mountIndex)
 {
     U32 cnt = system->MountCount;
     system->MountCount++;
+    if (system->NextIndex != UINT32_MAX)
+    {
+        system->Mounted[system->NextIndex] = mountPoint;
+        *mountIndex = system->NextIndex;
+        system->NextIndex = UINT32_MAX;
+        return MageTrue;
+    }
     system->Mounted = realloc(system->Mounted, sizeof(const char *) * cnt + 1);
     system->Mounted[cnt] = mountPoint;
     *mountIndex = cnt;
     return MageTrue;
 }
-
-
 U8 MageFileSystemReadMountedDirectory(MageFileSystem *system, const char *file, const U8 searchOverride, const U32 overrideIndex)
 {    
     FILE *f;
@@ -44,6 +50,29 @@ U8 MageFileSystemReadMountedDirectory(MageFileSystem *system, const char *file, 
     }
     MAGE_HANDLE_ERROR_MESSAGE(!foundFile, printf("Error: Unable to find requested [%s] file in filesystem\n", file));
     
+
+    return MageTrue;
+}
+U8 MageFileSystemUnmountDirectory(MageFileSystem *system, const char *mountPoint)
+{
+    MAGE_HANDLE_ERROR_MESSAGE(system->MountCount <= 0, printf("Error: Filesystem has not mounted directories active\n"));
+    U32 i;
+    
+    for (i = 0; i < system->MountCount; i++)
+    {
+        if (strcmp(mountPoint, system->Mounted[i]) == 0)
+            goto unmount;
+    }
+    
+    MAGE_HANDLE_ERROR_MESSAGE(0 == 0, printf("Error: Unable to find mounted directory of %s in filesystem\n", mountPoint));
+    
+
+    unmount:
+    {
+        system->NextIndex = i;
+        system->Mounted[i] = NULL;
+        system->MountCount--;
+    }
 
     return MageTrue;
 }
--- Mage/Source/FileSystem.h
+++ Mage/Source/FileSystem.h
@@ -7,12 +7,14 @@
 typedef struct MageFileSystem
 {
     const char **Mounted;
+    U32 NextIndex;
     U32 MountCount;
 } MageFileSystem;
 
 extern U8 MageFileSystemCreate(MageFileSystemCreateInfo *info, MageFileSystem *system);
 extern U8 MageFileSystemMountDirectory(MageFileSystem *system, const char *mountPoint, U32 *mountIndex);
 extern U8 MageFileSystemReadMountedDirectory(MageFileSystem *system, const char *file, const U8 searchOverride, const U32 overrideIndex);
+extern U8 MageFileSystemUnmountDirectory(MageFileSystem *system, const char *mountPoint);
 extern U8 MageFileSystemDestory(MageFileSystem *system);
 
 #endif
