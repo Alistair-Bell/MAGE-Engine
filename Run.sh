cf06b940133cd50ce91beca3d2d3f51bd6612ae3
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2020-12-30 21:11:42 +0000

    Allow for user to build release mode

--- Run.sh
+++ Run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 clear
-cmake -B Build -D PROJECT_BUILD_PLATFORM=Linux -D CMAKE_BUILD_TYPE=Debug -D PROJECT_BUILD_UNIT_TESTS=On
+
+if [ "$#" -le 0 ]; then
+    buildMode=Debug # automaticly use debug
+else
+    buildMode=$1
+fi
+
+cmake -B Build -D PROJECT_BUILD_PLATFORM=Linux -D CMAKE_BUILD_TYPE=$buildMode -D PROJECT_BUILD_UNIT_TESTS=On
+
 cd Build
 make
 make test
