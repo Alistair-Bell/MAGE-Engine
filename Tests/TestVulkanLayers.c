38ceb7ffec9b6bc8b3d081759179b34e93939970
Alistair <70592208+Alistair-Bell@users.noreply.github.com>
2021-01-18 12:27:20 +0000

    Fixing test
    Kinda broken

--- Tests/TestVulkanLayers.c
+++ Tests/TestVulkanLayers.c
@@ -1,20 +1,21 @@
 #include <Mage.h>
 
+#define STR_ARRAY_COUNT(arr) (sizeof(arr) / sizeof(const char *))
 
 static U8 TestValidationLayers(U0 *data)
 {
-    const char *layers[] = { VK_KHR_SURFACE_EXTENSION_NAME };
-    return MageVulkanRendererValidateExtensionsPresent(layers, sizeof(layers) / sizeof(const char *));
+    const char *layers[] = { "VK_LAYER_KHRONOS_validation"  };
+    return MageVulkanRendererValidateLayersPresent(layers, STR_ARRAY_COUNT(layers));
 }
 static U8 TestInstanceExtensions(U0 *data)
 {
     const char *extensions[] = { VK_KHR_SURFACE_EXTENSION_NAME };
-    return MageVulkanRendererValidateExtensionsPresent(extensions, sizeof(extensions) / sizeof(const char *));
+    return MageVulkanRendererValidateExtensionsPresent(extensions, STR_ARRAY_COUNT(extensions));
 }
 
 I32 main()
 {
-    MageUnitTestCreateInfo tests[1];
+    MageUnitTestCreateInfo tests[2];
     U32 testCount = (sizeof(tests) / sizeof(MageUnitTestCreateInfo));
     memset(&tests, 0, sizeof(tests));
     
@@ -25,12 +26,12 @@ I32 main()
     tests[0].Assertions             = MageFalse;
     tests[0].Callback               = TestValidationLayers;
 
-    tests[0].TestName               = "`Unit Tests` - vulkan instance extensions test";
-    tests[0].FailMessage            = "`Vulkan Instance Extensions` module has failed";
-    tests[0].ProgramData            = NULL;
-    tests[0].ExpectedResult         = MageTrue;
-    tests[0].Assertions             = MageFalse;
-    tests[0].Callback               = TestInstanceExtensions;
+    tests[1].TestName               = "`Unit Tests` - vulkan instance extensions test";
+    tests[1].FailMessage            = "`Vulkan Instance Extensions` module has failed";
+    tests[1].ProgramData            = NULL;
+    tests[1].ExpectedResult         = MageTrue;
+    tests[1].Assertions             = MageFalse;
+    tests[1].Callback               = TestInstanceExtensions;
 
     MageUnitTestRuntimeInfo runtime;
     return MageUnitTestRunTests(tests, testCount, &runtime);
